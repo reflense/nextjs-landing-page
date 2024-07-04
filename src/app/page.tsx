01d5f82538401f68f42532dfddfa028345f8624f
Tanay <tanaynaik0710@gmail.com>
2024-07-04 15:48:06 +0530

    responsivness

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -12,6 +12,7 @@ import illustration9 from "../../public/images/section 3 3.svg";
 import illustration10 from "../../public/images/section 3 4.svg";
 import illustration11 from "../../public/images/section 3 5.svg";
 import logo from "../../public/images/Reflense (1).svg";
+import "../app/style.css";
 
 
 export default function Home() {
@@ -29,8 +30,8 @@ export default function Home() {
 
       <section className="flex mt-10  w-screen px-20 mb-20">
         <div className="w-6/12  h-96 flex flex-col justify-center items-center gap-5">
-          <p className="text-center font-black  text-3xl">Expand your<br></br> recruitment network <br></br>and find the perfect fit</p>
-          <button className="bg-blue-600 text-2xl px-6 py-2 rounded-full text-white bold " style={{ backgroundColor: "#042D62" }}>Let's Start Today</button>
+          <p className="text-center font-black  2xl:text-3xl xl:text-2xl lg:text-lg md:text-base sm:text-sm">Expand your<br></br> recruitment network <br></br>and find the perfect fit</p>
+          <button className="bg-blue-600 2xl:text-2xl xl:text-lg lg-text-base md:text-sm sm:text-xs px-6 py-2 rounded-full text-white bold " style={{ backgroundColor: "#042D62" }}>Let's Start Today</button>
         </div>
         <div className="w-6/12  h-96 flex justify-center">
           <Image src="/images/section1 right.svg" alt="avc"
@@ -52,15 +53,17 @@ export default function Home() {
         <div className="mt-32 relative">
           <div className="flex justify-center items-center  py-8" style={{ position: "relative" }}>
             <Image src={illustration2} alt="Illustrations" width={0} height={0} sizes="100vw" style={{ width: '25%', height: '130%' }}></Image>
-            <Image src={illustration3} alt="Illustrations" width={0} height={0} sizes="100vw" style={{ width: '17%', height: '130%' }}></Image>
+             
+              <Image src={illustration3} alt="Illustrations" width={0} height={0} sizes="100vw" style={{width: '17%', height: '130%' }}></Image>
+            
             <Image src={illustration4} alt="Illustrations" width={0} height={0} sizes="100vw" style={{ width: '20%', height: '130%' }}></Image>
             <Image src={illustration5} alt="Illustrations" width={0} height={0} sizes="100vw" style={{ width: '17%', height: '130%' }}></Image>
             <Image src={illustration6} alt="Illustrations" width={0} height={0} sizes="100vw" style={{ width: '25%', height: '130%' }}></Image>
 
-            <p className="absolute bottom-6 left-28 font-bold">With our rockstar recruiter network</p>
-            <p className="absolute t-23 l-23 pl-10 text-center font-bold">Unlock access to a vast<br></br> pool of top-tier talent<br></br> that fits your needs</p>
-            <p className="absolute t-20 r-27 text-center font-bold">Discover top talent<br></br> efficiently, saving <br></br>time and energy</p>
-            <p className="absolute top-28 right-2 text-center font-bold text-sm">With ratings and skill <br></br>evaluations from trusted <br></br>and authentic recruiters</p>
+            <p className="absolute text-center   text-xs   font-bold t-28 l-26 text-[4px] sm:text-[6px] md:text-[8px]  lg:text-[10px] xl:text-[14px]" >Unlock access to a vast<br></br> pool of top-tier talent<br></br> that fits your needs</p>
+            <p className="absolute bottom-6 l-7 font-medium text-xs md:text-sm md:font-bold lg:text-base  xl:text-lg ">With our rockstar recruiter network</p>
+            <p className="absolute  r-27  t-26  text-center font-bold text-[4px] sm:text-[6px] md:text-[8px]  lg:text-[10px] xl:text-[14px] xl:t-26 2xl:t-22">Discover top talent<br></br> efficiently, saving <br></br>time and energy</p>
+            <p className="absolute t-22 r-2 text-center font-bold text-sm  text-[4px] sm:text-[6px] md:text-[8px]  lg:text-[10px] xl:text-[14px]">With ratings and skill <br></br>evaluations from trusted <br></br>and authentic recruiters</p>
           </div>
 
         </div>
@@ -75,19 +78,20 @@ export default function Home() {
       </section>
 
       <section className="min-h-screen">
-        <div className="flex mt-48 relative justify-between items-center px-10 mb-10"> 
+        <div className="flex mt-48 relative justify-between items-center px-10 mb-10">
           <Image src={illustration7} alt="Illustrations" width={0} height={0} sizes="100vw" style={{ width: '40%', height: '130%' }}></Image>
-          <Image src={illustration8} alt="Illustrations" width={0} height={0} sizes="100vw" style={{ width: '17%', height: '50%',left:"29%" ,top:"-24%"}} className="absolute "></Image>
-          <Image src={illustration9} alt="Illustrations" width={0} height={0} sizes="100vw" style={{ width: '20%', height: '30%',right:"27%" ,bottom:"28%" }} className="absolute"></Image>
+          <Image src={illustration8} alt="Illustrations" width={0} height={0} sizes="100vw" style={{ width: '17%', height: '50%', left: "29%", top: "-24%" }} className="absolute "></Image>
+          <Image src={illustration9} alt="Illustrations" width={0} height={0} sizes="100vw" style={{ width: '20%', height: '30%', right: "27%", bottom: "28%" }} className="absolute"></Image>
           <Image src={illustration10} alt="Illustrations" width={0} height={0} sizes="100vw" style={{ width: '40%', height: '130%' }}></Image>
-          <Image src={illustration11} alt="Illustrations" width={0} height={0} sizes="100vw" style={{ width: '25%', height: '130%' }} className="absolute"></Image>
+          <Image src={illustration11} alt="Illustrations" width={0} height={0} sizes="100vw" style={{ width: '20%', height: '100%', top: "-22%", right: "-4%" }} className="absolute"></Image>
 
-          <p className="absolute top-0 left-60 font-bold text-right">Get access to numerous job <br></br>opportunities</p>
-          <p className="absolute  text-center  w-full text-3xl font-bold" style={{ color:"#41757D", top:"30%"}} >Power to the Recruiters</p>
-          <p className="absolute b-20  text-center font-bold" style={{right:"40%"}}>With your expertise and our tools, ensuring every<br></br> talent you engage with gets a chance to shine.</p>
-          </div>
+          <p className="absolute top-0 l-17 font-bold text-right text-[6px] sm:text-[8px] md:text-[10px]  lg:text-[12px] xl:text-[20px]">Get access to numerous job <br></br>opportunities</p>
+          <p className="absolute  text-center  w-full text-3xl font-bold text-[12px] sm:text-[14px] md:text-[18px]  lg:text-[26px] xl:text-[30px]" style={{ color: "#41757D", top: "30%" }} >Power to the Recruiters</p>
+          <p className="absolute b-20  text-center font-bold text-[6px] sm:text-[8px] md:text-[10px]  lg:text-[12px] xl:text-[18px]" style={{ right: "40%" }}>With your expertise and our tools, ensuring every<br></br> talent you engage with gets a chance to shine.</p>
+          <p className="absolute b-20   text-right font-bold text-[6px] sm:text-[8px] md:text-[10px]  lg:text-[12px] xl:text-[18px]" style={{ right: "0%", top: "-14%" }}>You know your candidates better. Leverage it to find<br></br> perfect opportunities for them. Score based on job fit,<br></br> guiding effortlessly</p>
+        </div>
 
-          <div style={{backgroundColor:"#042D62"}} className="w-full py-10"><p className="text-center text-2xl font-bold text-white">Email us for any questions: info@reflesnse.com</p></div>
+        <div style={{ backgroundColor: "#042D62" }} className="w-full py-10"><p className="text-center text-2xl font-bold text-white">Email us for any questions: info@reflesnse.com</p></div>
       </section>
 
       {/* <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
